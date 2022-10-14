5d3602fba35084183dfc3fb7f2084121c09f92b4
Miroslava1301 <miroslavats5@gmail.com>
2022-10-14 12:15:55 +0200

    all in one

---
+++ TikTokFinalProject/src/main/java/com/example/tiktokfinalproject/TikTokFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiktokfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikTokFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikTokFinalProjectApplication.class, args);
+    }
+
+}
