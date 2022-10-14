849918ce786fcfcd3af87a6271b881b0270d58b1
Miroslava1301 <miroslavats5@gmail.com>
2022-10-14 17:12:17 +0200

    test users

--- TikTokFinalProject/src/main/java/com/tiktok/controller/UserController.java
+++ TikTokFinalProject/src/main/java/com/tiktok/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.tiktok.controller;
 
+import com.tiktok.model.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
+    private UserRepository userRepository;
     @GetMapping("/users")
     public String getAll() {
         return "MIraaaa poginahme";
     }
+
 
 
 
--- TikTokFinalProject/src/main/java/com/tiktok/model/entities/User.java
+++ TikTokFinalProject/src/main/java/com/tiktok/model/entities/User.java
@@ -1,4 +1,16 @@
 package com.tiktok.model.entities;
 
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table (name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
